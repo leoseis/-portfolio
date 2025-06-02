f3eeb90a907b57a9071f3271d804a7859d799e0e
leoseis <eboscrib2@yahoo.com>
2025-06-02 11:09:18 +0100

    fixed project file error

--- ebo/forms.py
+++ ebo/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True)
-    email = forms.EmailField(required=True)
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
     phone = forms.CharField(max_length=20, required=False)
-    msg = forms.CharField(widget=forms.Textarea, required=True)
+    msg = forms.CharField(widget=forms.Textarea)
