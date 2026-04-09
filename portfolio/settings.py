ede8def507af242a3cf59d1977a09420b5206328
leoseis <eboscrib@yahoo.com>
2026-04-09 11:53:59 +0100

    fixxed some venv heavy files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -158,11 +158,11 @@ DATABASES = {
 }
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
-DEBUG = os.environ.get("DEBUG", "False") == "True"
-AALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 STATIC_URL = 'static/'
 
