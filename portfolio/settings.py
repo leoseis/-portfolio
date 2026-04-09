e9afd619ae1da4c601b3b78b2f35244fc0d6033c
leoseis <eboscrib@yahoo.com>
2026-04-09 11:35:44 +0100

    clean repository and apply gitignore

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -151,9 +151,11 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
